63f09521c57c376a9843aba1da02e70726d6cdb1
Kevin Fernando <kevinfernandocahyadi_2226240053@mhs.mdp.ac.id>
2024-11-13 20:27:24 +0700

    lanjutan angular

---
+++ belajar_Angular/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <section>
+      <img class="listing-photo" scr="" alt="Exterior photo of...">
+      <h2 class="Listing-heading">TITTLE</h2>
+      <p class="listing-location">Citym sates</p>
+      </section>
+  `,
+      styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
