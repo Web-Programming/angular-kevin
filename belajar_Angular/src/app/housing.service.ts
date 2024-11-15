49a9074361ad81496116cc55199e1238af2d5494
KevinFernando24 <kevinfernandocahyadi_2226240053@mhs.mdp.ac.id>
2024-11-15 18:28:55 +0700

    update

--- belajar_Angular/src/app/housing.service.ts
+++ belajar_Angular/src/app/housing.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  protected housingLocationList: HousingLocation[] = []
 
   constructor() { }
+
+  getAllHousingLocation() : HousingLocation[]{
+    return this.housingLocationList;
+  }
+  getHousingLocationById (id: Number): HousingLocation | undefined{
+    return this.housingLocationList find(housingLocaton =>)
+  }
 }
+
+
