a331c14fa682c3c22a557699a7820e4f3ba6fd7b
KevinFernando24 <kevinfernandocahyadi_2226240053@mhs.mdp.ac.id>
2024-11-15 18:07:58 +0700

    angular

--- belajar_Angular/src/app/housing-location/housing-location.component.ts
+++ belajar_Angular/src/app/housing-location/housing-location.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component , Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   template: `
-    <section>
-      <img class="listing-photo" scr="" alt="Exterior photo of...">
-      <h2 class="Listing-heading">TITTLE</h2>
-      <p class="listing-location">Citym sates</p>
-      </section>
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['details',housingLocation.id]">Show More</a>
+    </section>
   `,
-      styleUrl: './housing-location.component.css'
+  styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation
 }
